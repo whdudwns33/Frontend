189fca0df7283d99c5026e0fbc91b798234fa2c5
jojo6807 <jyj051811@gmail.com>
2023-11-20 23:26:11 +0900

    API 느낌옴

--- React/test-pro/src/App.js
+++ React/test-pro/src/App.js
@@ -1,28 +1,10 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import React from 'react';
+import StockInfo from './component';
+
 function App() {
-  const [stockPrice, setStockPrice] = useState("");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:8111/api/finance/stockPrice"
-        );
-        console.log("불려 왔는지?");
-        setStockPrice(response.data);
-      } catch (error) {
-        console.error("Error fetching stock price:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <div>
-      <h1>Stock Price</h1>
-      <p>{stockPrice}</p>
+    <div className="App">
+      <StockInfo />
     </div>
   );
 }
