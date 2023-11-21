0d36d1b58ce952a3b4228a7c65152dbe52cb5b85
jyj051811@gmail.com <jyj051811@gmail.com>
2023-11-21 18:21:39 +0900

    주식 크롤링

--- React/test-pro/src/App.js
+++ React/test-pro/src/App.js
@@ -1,30 +1,36 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-function App() {
-  const [stockPrice, setStockPrice] = useState("");
+
+const App = () => {
+  const [stockPrice, setStockPrice] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:8111/api/finance/stockPrice"
-        );
-        console.log("불려 왔는지?");
-        setStockPrice(response.data);
-      } catch (error) {
-        console.error("Error fetching stock price:", error);
-      }
-    };
-
-    fetchData();
+    // 서버에서 주식 가격 데이터 가져오기
+    fetchStockPrice();
   }, []);
+
+  const fetchStockPrice = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/api/stock/price"); // 서버의 엔드포인트로 수정
+      const data = response.data;
+      console.log(data);
+      // 가져온 데이터를 상태에 설정
+      setStockPrice(data.price);
+    } catch (error) {
+      console.error("Error fetching stock price:", error);
+    }
+  };
 
   return (
     <div>
-      <h1>Stock Price</h1>
-      <p>{stockPrice}</p>
+      <h1>네이버 주식 가격</h1>
+      {stockPrice !== null ? (
+        <p>현재 가격: {stockPrice} 원</p>
+      ) : (
+        <p>가격을 불러오는 중...</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ React/test-pro/src/axios/Axios.jsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const Axios = {
+  Info: async,
+};
+
+export default Axios;
